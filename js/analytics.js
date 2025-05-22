af371ff929f3aabd668e730887bdc6336e33c541
yuridmayerpro <yurid.mayer.pro@gmail.com>
2025-05-22 15:49:31 -0300

    Deployed 324a3fe with MkDocs version: 1.6.1

---
+++ js/analytics.js
@@ -0,0 +1,7 @@
+// js/analytics.js
+window.dataLayer = window.dataLayer || [];
+function gtag(){ dataLayer.push(arguments); }
+gtag('js', new Date());
+
+// usa o seu Measurement ID
+gtag('config', 'G-EJ9HJJYV43');
